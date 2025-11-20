61f345001237221a79cc9aa50b35704a411422b9
AsqarUaHa <94336900+AsqarUaHa@users.noreply.github.com>
2025-11-21 02:07:53 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ set -e
 
 cd backend
 
-# На всякий случай выводим версию Python, чтобы видеть, что он есть
+# Просто на всякий случай покажем версию Python в логах
 python --version || python3 --version
 
-# Запуск через модуль, а не через бинарь uvicorn
+# ВАЖНО: запуск через python -m uvicorn, а не просто "uvicorn"
 python -m uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
